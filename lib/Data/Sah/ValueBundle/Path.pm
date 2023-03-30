97b49790c2d79f2b2fbcc624c74387d8b5e98005
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-03-30 21:54:55 +0700

    Initial commit

---
+++ lib/Data/Sah/ValueBundle/Path.pm
@@ -0,0 +1,17 @@
+package Data::Sah::ValueBundle::Path;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Default value rules related to paths
+
+=head1 SEE ALSO
+
+L<Data::Sah::DefaultValue>
+
+L<Data::Sah>
